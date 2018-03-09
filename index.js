4324277be15e261bc81c1aed01abb7e09e5c1631
k4m4 <nikolaskam@gmail.com>
2018-03-09 14:18:10 +0200

    Fix regex

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ module.exports = function (str, opts) {
 		throw new TypeError('Expected a string');
 	}
 
-	return condenseWhitespace(str).match(/[a-zA-Z0-9_\u0392-\u03c9\u0400-\u04FF]+|[\u4E00-\u9FFF\u3400-\u4dbf\uf900-\ufaff\u3040-\u309f\uac00-\ud7af\u0400-\u04FF]+|[\u00E4\u00C4\u00E5\u00C5\u00F6\u00D6]+|\w+/g).length;
+	return condenseWhitespace(str).match(/\S+/g).length;
 };
